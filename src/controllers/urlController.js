90d6cc91695aeb4799134875a8680f81037c6843
gaganturka <gaganturka1@gmail.com>
2022-04-24 21:28:06 +0530

    completed

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,6 +1,6 @@
-// const validUrl = require('valid-url')
-const shortId = require('shortid')
 const urlModel = require('../models/urlModel')
+
+const shortId = require('shortid')
 const redis = require("redis");
 const { promisify } = require("util");
 
@@ -49,12 +49,17 @@ const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 const shortUrl = async function (req, res) {
     try {
 
-        const longUrl = req.body.longUrl
+        const requestBody = req.body
+        const longUrl = requestBody.longUrl.trim()
         const query = req.query
   
 
         if (!queryparams(query)) {
-            return res.status(400).send({ status: false, message: 'invalid request' })
+            return res.status(400).send({ status: false, message: 'page Not found' })
+        }
+
+        if(Object.keys(requestBody).length != 1){
+            return res.status(400).send({status : false, message :'invalid entery in request body'})
         }
 
         //here isvalid mainly using for if type != 'string' then it give error
@@ -74,18 +79,18 @@ const shortUrl = async function (req, res) {
             } else {
                 const shortUrl = baseUrl + '/' + urlCode
 
-                // decreasing db call (do not using create method)
                 url = new urlModel({
                     longUrl,
                     shortUrl,
                     urlCode
                 })
+                // OR
                 // const createUrl = await urlModel.create(url)  this db call we neglected it
 
                 await url.save()
 
                 if (url) {
-                    await SET_ASYNC(`${urlCode}`, JSON.stringify(url), 'Ex', 60)
+                    await SET_ASYNC(`${urlCode}`, JSON.stringify(url), 'Ex', 600) //Ex define expire time of catch memory in sec.
                 }
                 const response = {
                     longUrl: url.longUrl,
@@ -113,7 +118,7 @@ const longUrl = async function (req, res) {
         const query = req.query
 
         if (!queryparams(query)) {
-            return res.status(400).send({ status: false, message: 'invalid request' })
+            return res.status(400).send({ status: false, message: 'Page not found' })
         }
         let caching = await GET_ASYNC(`${urlCode}`)
 
@@ -125,7 +130,7 @@ const longUrl = async function (req, res) {
             const urlCodeExist = await urlModel.findOne({ urlCode })
 
             if (urlCodeExist) {
-                await SET_ASYNC(`${(urlCode)}`, JSON.stringify(urlCodeExist), 'Ex', 60)
+                await SET_ASYNC(`${(urlCode)}`, JSON.stringify(urlCodeExist), 'Ex', 600)
                 res.redirect(302, urlCodeExist.longUrl)
             }
             else {
